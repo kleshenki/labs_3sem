07060fde4cb8d39b1244768a5f782f8838c8fecf
Crawler <Crawler@DESKTOP-B414NCN>
2020-10-15 13:15:28 +0500

    Добавим юнит тесты

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#define protected public
+#include "..\String\String.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+		friend class String;
+	public:
+		TEST_METHOD(StringLength)
+		{
+			String* str = new String(50, "Test string");
+			Assert::AreEqual(11, str->RealLength());
+		}
+
+		TEST_METHOD(StringConcat)
+		{
+			String* str = new String(50, "Starting string");
+			char* append = " appended";
+			*str + append;
+			Assert::AreEqual("024Starting string appended", str->arr);
+
+		}
+
+		TEST_METHOD(ResizeStringLength)
+		{
+			String *str = new String(50, "New string");
+			String *append = new String(40, " appended");
+			*str + *append;
+			Assert::AreEqual(str->RealLength(), 19);
+		}
+	};
+}
